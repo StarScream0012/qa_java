fef5068dbd7196f3aaa42a9eee5e7b363133aab2
Mansur Akhmetzhan <ahmetzan.m@gmail.com>
2024-10-20 22:33:28 +0500

    Project 6 sprint

---
+++ src/test/java/com/example/CatTest.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class CatTest {
+    @Mock
+    Feline feline;
+    @Test
+    public void getSoundTest(){
+        Cat cat = new Cat(feline);
+        String expected = "Мяу";
+        String actual = cat.getSound();
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void getFoodTest() throws Exception {
+        Cat cat = new Cat(feline);
+        cat.getFood();
+        Mockito.verify(feline).eatMeat();
+    }
+}
---
+++ src/test/java/com/example/FelineTest.java
@@ -0,0 +1,37 @@
+package com.example;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import java.util.List;
+
+
+public class FelineTest {
+
+    @Test
+    public void eatMeatTest(){
+        Feline feline = new Feline();
+        List expected = List.of("Животные", "Птицы", "Рыба");
+        try {
+            List actual = feline.eatMeat();
+            Assert.assertEquals(expected, actual);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    @Test
+    public void getFamilyTest(){
+        Feline feline = new Feline();
+        String expected = "Кошачьи";
+        String actual = feline.getFamily();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void getKittensTest(){
+        Feline feline = new Feline();
+        int expected = 1;
+        int actual = feline.getKittens();
+        Assert.assertEquals(expected, actual);
+    }
+}
