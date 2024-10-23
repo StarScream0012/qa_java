7ea90aaaa9bf32d6566e50f015ed6db216e636b9
Mansur Akhmetzhan <ahmetzan.m@gmail.com>
2024-10-23 19:14:56 +0500

    Project 6 sprint

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,10 +1,14 @@
 package com.example;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LionTest {
@@ -23,4 +27,26 @@ public class LionTest {
         lion.getKittens();
         Mockito.verify(feline).getKittens();
     }
+    @Test
+    public void femaleLionShouldNotHaveMane() throws Exception {
+        Lion lion = new Lion("Самка", feline);
+        assertFalse(lion.doesHaveMane());
+    }
+    @Test
+    public void maleLionShouldHaveMane() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        assertTrue(lion.doesHaveMane());
+    }
+    @Test(expected = Exception.class)
+    public void testException() throws Exception {
+        Lion lion = new Lion("Другое", feline);
+        lion.doesHaveMane();
+    }
+    @Test
+    public void getKittensShouldReturnNumberOfKittens() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        when(feline.getKittens()).thenReturn(3);
+        assertEquals(3, lion.getKittens());
+    }
+
 }
